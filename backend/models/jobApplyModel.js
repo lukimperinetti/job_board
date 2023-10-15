7aed3338163bfa3709d85bc8558b9c6ed45c6adf
lukimperinetti <luk.imperinetti@epitech.eu>
2023-10-15 18:17:49 +0200

    Status ok, seen, applied & stored

---
+++ backend/models/jobApplyModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const jobApplySchema = new Schema({
+    jobId: {
+        type: String,
+        required: true
+    },
+    userId:{
+        type: String,
+        required: true
+    },
+    applied:{
+        type: Boolean,
+        required: false
+    },
+    stored:{
+        type: Boolean,
+        required: false
+
+    },
+    seen:{
+        type: Boolean,
+        required: false
+
+    }
+},{ timestamps: true})    //associated timestamp information indicating when it was created or last updated
+
+module.exports = mongoose.model('jobApply', jobApplySchema)
+
