e4155714b9a3217f51df18afa6ff550aa1cd3f8a
Manalbariz <manal.bariz@epitech.eu>
2023-10-22 12:38:14 +0200

    StyleModif

--- frontend/src/components/JobsDetails.jsx
+++ frontend/src/components/JobsDetails.jsx
@@ -7,19 +7,19 @@ const JobsDetails = ({ job }) => {
     const elementId = event.target.getAttribute("id"); // on reccupère l'id au click
   };
   return (
-    <div className="job-details" id={job._id}>
-      <div className="courses-container">
-        <div className="course">
-          <div className="course-preview">
-            <h6>{job.Contract}</h6>
-            <h2>{job.Title}</h2>
+    <div className="job-details" id={job._id} >
+      <div className="courses-container" >
+        <div className="course" >
+          <div className="course-preview" >
+            <h6 style={{animation: "fadeIn 1s ease-in-out", fontSize: "10px"}}>{job.Contract}</h6>
+            <h2 style={{animation: "fadeIn 1s ease-in-out"}}>{job.Title}</h2>
           </div>
           <div className="course-info">
-            <h6>{job.Localisation}</h6>
-            <h2>{descriptionLimitee}...</h2>
+            <h6 style={{animation: "fadeIn 1s ease-in-out", fontSize: "10px"}}>{job.Localisation}</h6>
+            <h2 style={{animation: "fadeIn 1s ease-in-out"}}>{descriptionLimitee}...</h2>
 
             <Link to={`/jobs/${job._id}`}>
-              <button className="btn" onClick={handleButtonClick}>
+              <button className="btn" onClick={handleButtonClick} style={{animation: "fadeIn 1s ease-in-out"}}>
                 Voir plus
               </button>
             </Link>
